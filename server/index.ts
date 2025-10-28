64f5a98b35f48ddeb000f69c1d9ac6f00cdfc9b0
jayanth922 <jayanth.kalyanam@gmail.com>
2025-10-28 01:29:45 -0700

    fix: force fresh Vercel build

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { type Request, Response, NextFunction } from "express";
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
@@ -13,21 +14,21 @@ declare module 'http' {
   }
 }
 app.use(express.json({
-  verify: (req, _res, buf) => {
+  verify: (req: Request, _res: Response, buf: Buffer) => {
     req.rawBody = buf;
   }
 }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   const start = Date.now();
   const path = req.path;
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
   const originalResJson = res.json;
-  res.json = function (bodyJson, ...args) {
+  res.json = function (bodyJson: any, ...args: any[]) {
     capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
+    return originalResJson.call(res, bodyJson);
   };
 
   res.on("finish", () => {
